4b3756fea458e5b8785138e00f33bd92edbc820c
Payam1991Gr <pgerackoohi@gmail.com>
2020-09-03 19:13:48 +0430

    AbdroidTest issue is resolved.

--- app/src/androidTest/java/com/aachartmodel/aainfographics/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/aachartmodel/aainfographics/ExampleInstrumentedTest.kt
@@ -1,7 +1,7 @@
 package com.aachartmodel.aainfographics
 
-import android.support.test.InstrumentationRegistry
-import android.support.test.runner.AndroidJUnit4
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.platform.app.InstrumentationRegistry
 
 import org.junit.Test
 import org.junit.runner.RunWith
@@ -17,8 +17,7 @@ import org.junit.Assert.*
 class ExampleInstrumentedTest {
     @Test
     fun useAppContext() {
-        // Context of the app under test.
-        val appContext = InstrumentationRegistry.getTargetContext()
-        assertEquals("com.aachartmodel.aainfographics", appContext.packageName)
+        val appContext = InstrumentationRegistry.getInstrumentation().targetContext
+        assertEquals("com.payam1991gr.aachartlib.test", appContext.packageName)
     }
 }
